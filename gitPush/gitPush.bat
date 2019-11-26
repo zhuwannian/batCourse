b4c749ddf5eb423eedd96efd63a0219e20048256
zhuwannian <zhu_wannian@163.com>
2019-11-25 22:34:51 -0800

    bat实战教程git代码提交

--- gitPush/gitPush.bat
+++ gitPush/gitPush.bat
@@ -1,5 +1,9 @@
 @echo off
 
+set gitMaster=0 
+set isRun=0 
+set gitPushBranchName=
+
 echo "git status"
 git status
 
@@ -13,4 +17,35 @@ set /p gitRemark=Please input git commit remark:
 echo gitRemark %gitRemark%
 git commit -m %gitRemark%
 
+for /F %%m in ('git symbolic-ref --short HEAD') do ( set gitBranch=%%m)
+echo current branch name : %gitBranch%
 
+if "%gitBranch%"=="master" (
+	echo yes
+	if "%gitMaster%"=="1" (
+		set gitPushBranchName= 
+		set isRun=1
+	) else (
+		echo 默认是不允许提交master的代码，如果想提交master的代码请gitMaster的值改成1
+		set isRun=0
+		pause
+		rem exit
+	)
+) else (
+	echo NO
+	:: set gitBranch=zwndev_myCode19.11.1
+	set prefixEmpty=%gitBranch:zwndev_=%
+	:: set prefixEmpty=myCode19.11.1
+	set gitPushBranchName=%prefixEmpty%
+	set isRun=1
+
+)
+
+echo gitPushBranchName %gitPushBranchName%
+echo "-isRun-"%isRun%
+if "%isRun%"=="1" (
+	echo "git push origin %gitPushBranchName%"
+	git push origin %gitPushBranchName%
+) else (
+	pause
+)
