bb73a719026355a35b60b1eb5fb4aa59b4cd0a72
zhuwannian <zhu_wannian@163.com>
2019-11-25 22:29:41 -0800

    1

---
+++ gitPush/gitPush.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo "git status"
+git status
+
+:: input commit message
+set /p gitAddFile=Please input git add file:
+echo gitAddFile %gitAddFile%
+git add %gitAddFile%
+
+:: input commit message
+set /p gitRemark=Please input git commit remark:
+echo gitRemark %gitRemark%
+:: git commit -m %gitRemark%
