a5a2dffba60a2a88640f6495b2b027386266e8dd
朱万年 <zwn@acewill.cn>
2019-12-20 19:04:38 +0800

    ----

---
+++ gitClone/gitCloneAlishopVue.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set gitCloneBranchBase=T19113
+set gitCloneUrlBase=git@gitlab.acewill.cn:baiweilian/alishop.git
+set prefixName=zwndev
+set repository=AlishopVue
+set codeUrlBase=%repository%_%gitCloneBranchBase%
+set prefix=%prefixName%%repository%
+
+echo 'input branch: '%gitCloneBranchBase%
+
+echo 'input git clone url: '%gitCloneUrlBase%
+
+echo 'input save code url: '%codeUrlBase%
+
+echo "git clone -b %gitCloneBranchBase% %gitCloneUrlBase% %codeUrlBase%"
+git clone -b %gitCloneBranchBase% %gitCloneUrlBase% %codeUrlBase%
+
+cd %codeUrlBase%
+
+
+for /F %%j in ("%USERNAME%") do ( set userName=%%j)
+
+for /F %%j in ("%COMPUTERNAME%") do ( set computerName=%%j)
+
+echo "git checkout -b %prefix%_%gitCloneBranchBase%_%userName%_%computerName%"
+git checkout -b %prefix%_%gitCloneBranchBase%
+
+echo "git branch"
+git branch
+
+pause
