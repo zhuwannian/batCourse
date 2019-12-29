68309bbdba7b6738391a545dc2c8a7fb4ac11af5
zhuwannian <zhu_wannian@163.com>
2019-12-29 11:48:42 +0800

    gitPushMaster提交

--- gitPush/gitPushMaster.bat
+++ gitPush/gitPushMaster.bat
@@ -2,7 +2,7 @@
 
 :: git add -A  提交所有变化
 :: git add -u  提交被修改(modified)和被删除(deleted)文件，不包括新文件(new)
-:: git add .  提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件
+:: git add ./  提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件
 
 git add -A
 
