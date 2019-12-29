b4114c20229f18414130f8cdd00a92c30c68a630
zhuwannian <zhu_wannian@163.com>
2019-12-29 11:43:36 +0800

    -- ++

---
+++ gitPush/gitPushMaster.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: git add -A  提交所有变化
+:: git add -u  提交被修改(modified)和被删除(deleted)文件，不包括新文件(new)
+:: git add .  提交新文件(new)和被修改(modified)文件，不包括被删除(deleted)文件
+
+git add -A
+
+set commitMessage=-- ++
+
+git commit -m "%commitMessage%"
+
+git push origin
+
+pause
